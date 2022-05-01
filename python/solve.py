9ca41efa95ec47db1d6ae4537957acc7762f7fcc
jason-zhao-ctrl <zhaojingqiao666@berkeley.edu>
2022-05-01 16:09:28 -0700

    commit

--- python/solve.py
+++ python/solve.py
@@ -5,6 +5,7 @@ Modify this file to implement your own solvers.
 For usage, run `python3 solve.py --help`.
 """
 import argparse
+import random
 from asyncio.windows_events import NULL
 from ctypes.wintypes import BOOLEAN
 from json.encoder import INFINITY
@@ -111,10 +112,104 @@ def solve_greedy(instance: Instance) -> Solution:
         towers=towers
     )
  
+def solve_random(instance: Instance, varim: float = 0.75, varip:float = 1.2) -> Solution:
+    # find index of the max value in service matrix
+    def get_max_tower_rand(dim):
+        max_tower = [Point(0, 0)]
+        max_val = 0
+        for i in range(dim):
+            for j in range(dim):
+                if service_matrix[i][j] > max_val:
+                    max_val = service_matrix[i][j]
+        lower = int(varim * max_val)
+        if lower == 0:
+            lower = lower + 1
+        for i in range(dim):
+            for j in range(dim):
+                if service_matrix[i][j] >= lower:
+                    max_tower.append(Point(i, j))
+        return max_tower
+    
+    def get_minp_tower_rand(towerlist):
+        min = INFINITY
+        min_t = [towerlist[0]]
+        for t in towerlist:
+            x = t.x
+            y = t.y
+            if penalty_matrix[x][y] < min:
+                min = penalty_matrix[x][y]
+                min_t = [t]
+            elif penalty_matrix[x][y] == min:
+                min_t.append(t)
+        return min_t
+
+
+
+    # get cities covered by a service tower
+    def get_cities_covered(tower, cities, r, dim):
+        l = []
+        x = tower.x
+        y = tower.y
+        for i in range(max(0, x - r), min(dim, x + r + 1)):
+            for j in range(max(0, y - r), min(dim, y + r + 1)):
+                c = Point(i, j)
+                if c in cities and tower.distance_sq(c) <= r ** 2:
+                    l.append(c)
+        return l
+    
+    # update service matrix with val
+    def update_service(cities, r, val):
+        for city in cities:
+            x = city.x
+            y = city.y
+            for i in range(max(0, x - r), min(dim, x + r + 1)):
+                for j in range(max(0, y - r), min(dim, y + r + 1)):
+                    c = Point(i, j)
+                    if city.distance_sq(c) <= r ** 2:
+                        service_matrix[i][j] += val
+    
+    def update_penalty(rp, tower):
+       x = tower.x
+       y = tower.y
+       for i in range(max(0, x - rp), min(dim, x + rp + 1)):
+            for j in range(max(0, y - rp), min(dim, y + rp + 1)):
+                c = Point(i, j)
+                if tower.distance_sq(c) <= rp ** 2:
+                    penalty_matrix[i][j] += 1
+    
+
+    # main
+    dim = instance.D
+    r = instance.R_s
+    r_p = instance.R_p
+    cities = instance.cities.copy()  # shallow copy to prevent modifying cities directly
+    towers = []  # list to return
+ 
+    # initialize matrix with zeros
+    service_matrix = [[0] * instance.D for _ in range(instance.D)]
+    penalty_matrix = [[0] * instance.D for _ in range(instance.D)]
+    # fill matrix with service coverage by incrementing service areas of each city +1
+    update_service(cities, r, 1)
+ 
+    while len(cities) > 0:
+        max_towers = get_max_tower_rand(dim)
+        minp_tower = random.choice(get_minp_tower_rand(max_towers))
+        towers.append(minp_tower)
+        update_penalty(r_p, minp_tower)
+        covered = get_cities_covered(minp_tower, cities, r, dim)
+        update_service(covered, r, -1)  # decrement service areas of each covered city -1
+        cities = [c for c in cities if c not in covered]  # pop covered cities from cities
+    
+    
+    return Solution(
+        instance=instance,
+        towers=towers
+    )
  
 SOLVERS: Dict[str, Callable[[Instance], Solution]] = {
     "naive": solve_naive,
-    "greedy": solve_greedy
+    "greedy": solve_greedy,
+    "random": solve_random
 }
  
  
